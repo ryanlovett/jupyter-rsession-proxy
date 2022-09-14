f58f8e13ae5bdc5407a8f13fe072f33da9638e36
Ryan Lovett <rylo@berkeley.edu>
2022-09-13 17:16:24 -0700

    Remove auth-sign-in rewrite.

--- jupyter_rsession_proxy/__init__.py
+++ jupyter_rsession_proxy/__init__.py
@@ -31,18 +31,6 @@ def get_icon_path():
         os.path.dirname(os.path.abspath(__file__)), 'icons', 'rstudio.svg'
     )
 
-def rewrite_auth(response, request):
-    '''
-       As of rstudio-server 1.4ish, it would send the client to /auth-sign-in
-       rather than what the client sees as the full URL followed by
-       /auth-sign-in. See rstudio/rstudio#8888. We rewrite the response by
-       sending the client to the right place.
-    '''
-    for header, v in response.headers.get_all():
-        if header == "Location" and v.startswith("/auth-sign-in"):
-            # Visit the correct page
-            u = urlparse(request.uri)
-            response.headers[header] = urlunparse(u._replace(path=u.path+v))
 
 def get_system_user():
     try:
@@ -113,7 +101,6 @@ def setup_rserver():
         'command': _get_cmd,
         'timeout': _get_timeout(),
         'environment': _get_env,
-        'rewrite_response': rewrite_auth,
         'launcher_entry': {
             'title': 'RStudio',
             'icon_path': get_icon_path()
